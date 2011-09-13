591ee309be33cfb2495eb03b3de0f0d7da0bfffe
Oldrich Vetesnik <mrmil@igloonet.cz>
2011-09-13 12:52:47 +0200

    Fixed YAML and added Gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+# Required
+gem 'trollop'  # Options parsing
+gem 'highline' # Command line tools
+
+# Optional
+gem 'pry'      # Debugging, also comment in highway/highway.rb on line 2
--- highway.rb
+++ highway.rb
@@ -1,13 +1,14 @@
-autoload :YAML,					'yaml'
+require 'yaml'
+require 'pry' # Debugging
 
 module Highway
-	autoload :Array,			'./highway/core_ext/array'
-	autoload :Game,				'./highway/game'
-	autoload :Road,				'./highway/road'
-	autoload :Car,				'./highway/car'
-	autoload :Piece,			'./highway/piece'
-	autoload :Canvas,			'./highway/canvas'
-	autoload :Map,				'./highway/map'
+	autoload :Array,			      './highway/core_ext/array'
+	autoload :Game,				      './highway/game'
+	autoload :Road,				      './highway/road'
+	autoload :Car,				      './highway/car'
+	autoload :Piece,			      './highway/piece'
+	autoload :Canvas,			      './highway/canvas'
+	autoload :Map,				      './highway/map'
 	autoload :NoFuelException,	'./highway/exceptions'
 	autoload :NoRoadException,	'./highway/exceptions'
 end
